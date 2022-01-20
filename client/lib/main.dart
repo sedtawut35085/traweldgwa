34c948341da141b7e1cb32f9bb37cf0d3f9eb0ba
Sedtawut <sedtawut.sc@mail.kmutt.ac.th>
2022-01-20 10:41:49 +0700

    first commit

---
+++ client/lib/main.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:se_app2/navigator/nav/profile/profile.dart';
+import 'package:se_app2/navigator/nav.dart';
+import 'Home/shuttle/shuttle.dart';
+import 'navigator/nav.dart';
+import 'package:se_app2/Home/hotel/hotel.dart';
+
+void main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor:
+          SystemUiOverlayStyle.dark.systemNavigationBarColor,
+    ),
+  );
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MaterialApp(home: shuttle()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'traveldgwa',
+      routes: <String, WidgetBuilder>{
+        '/Navi': (BuildContext context) => Nav(),
+        '/airport_shuttle': (BuildContext context) => shuttle(),
+        // '/hotel ': (BuildContext context) => hotel(),
+        '/profile': (BuildContext context) => profile(),
+      },
+    );
+  }
+}
